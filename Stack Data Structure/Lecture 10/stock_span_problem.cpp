59e41f201bd7d3b6152f4fd8b8b46b3b41a7c4d2
Piyush Sharma <piyushsharma220699@gmail.com>
2020-10-21 02:48:26 +0530

    Added stock_span_problem.cpp

--- Stack Data Structure/Lecture 10/stock_span_problem.cpp
+++ Stack Data Structure/Lecture 10/stock_span_problem.cpp
@@ -1,7 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> stockSpanProblem(vector<int>& v1)
+{
+    stack<pair<int,int>> s1;
+    vector<int> answer;
+
+    for(int i=0; i<v1.size(); i++)
+    {
+        int temp;
+        if(s1.empty())
+            temp=1;
+        
+        else if(s1.top().first>v1[i])
+            temp=1;
+        
+        else if(s1.top().first<=v1[i])
+        {
+            while(!s1.empty() && s1.top().first<=v1[i])
+                s1.pop();
+            
+            if(s1.empty())
+                temp=i+1;
+            else
+                temp=i-s1.top().second;
+            
+        }
+
+        answer.push_back(temp);
+        s1.push({v1[i],i});
+    }
+
+    return answer;
+}
+
 int main()
 {
+    vector<int> v1{100, 80, 60, 70, 60, 75, 85};
+
+    cout<<"Printing the elements of vector:";
+    for(int i=0; i<v1.size(); i++)
+        cout<<" "<<v1[i];
+    cout<<endl;
+
+    vector<int> v2;
+    v2=stockSpanProblem(v1);
+
+    cout<<"Printing Span of Every Element: ";
+    for(int i=0; i<v2.size(); i++)
+        cout<<v2[i]<<" ";
+
     return 0;
 }
